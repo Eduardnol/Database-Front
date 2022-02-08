04528ef942a8ed2cfe80620213c1bada361f16c7
eduardnol <eduardo.nolla@outlook.com>
2022-02-08 11:57:48 +0100

    Migrated to support multiple pages

--- src/pages/home/App.vue
+++ src/pages/home/App.vue
@@ -17,8 +17,8 @@
 </template>
 
 <script>
-import Person from "./components/all_users/Person.vue";
-import Search from "./components/all_users/Search.vue";
+import Person from "../../components/all_users/Person.vue";
+import Search from "../../components/all_users/Search.vue";
 
 export default {
   name: "App",
@@ -35,7 +35,7 @@ export default {
 </script>
 
 <style>
-@import "./css/normalize.css";
+@import "../../css/normalize.css";
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
 .list_item {
