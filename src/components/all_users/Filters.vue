aab613cbfa4054e5aed0f499597ff9e84dd8fc8f
eduardnol <eduardo.nolla@outlook.com>
2022-02-08 14:36:31 +0100

    Grid ad main distribution of web page

---
+++ src/components/all_users/Filters.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="allfilters">
+    <p>Fecha Inicial</p>
+    <input v-model="beginDateBirthday" type="date" />
+
+    <p>Fecha Final</p>
+    <input v-model="endDateBirthday" type="date" />
+
+    <p>Fecha</p>
+    <input v-model="dateSaint" type="date" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filter",
+  data() {
+    return {
+      beginDateBirthday: "",
+      endDateBirthday: "",
+      dateSaint: "",
+    };
+  },
+};
+</script>
+<style scoped>
+.allfilters {
+  display: grid;
+  grid-template-columns: repeat(2, 180px);
+  grid-column-gap: 20px;
+  grid-row-gap: 10px;
+}
+.field {
+  display: flex;
+  flex-direction: row;
+}
+</style>
--- src/pages/home/App.vue
+++ src/pages/home/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="component">
-    <Search />
-
+    <div class="search">
+      <Search />
+    </div>
+    <div class="filters">
+      <Filters />
+    </div>
     <ul class="person_grid scrollable">
       <li class="list_item" v-for="person in persons" :key="person.id">
         <Person
@@ -20,6 +24,7 @@
 <script>
 import Person from "../../components/all_users/Person.vue";
 import Search from "../../components/all_users/Search.vue";
+import Filters from "../../components/all_users/Filters.vue";
 
 export default {
   name: "App",
@@ -31,6 +36,7 @@ export default {
   components: {
     Person,
     Search,
+    Filters,
   },
 };
 </script>
@@ -39,8 +45,20 @@ export default {
 @import "../../css/normalize.css";
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
+.component {
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+}
+.filters {
+  grid-area: 2 / 5 / 6 / 6;
+}
 .list_item {
   list-style-type: none;
+}
+.search {
+  grid-area: 1 / 5 / 2 / 6;
 }
 #app {
   font-family: Montserrat, sans-serif;
@@ -57,6 +75,7 @@ export default {
   grid-row-gap: 10px;
 }
 .scrollable {
+  grid-area: 2 / 1 / 6 / 3;
   height: 700px; /* or any value */
   overflow-y: auto;
 }
