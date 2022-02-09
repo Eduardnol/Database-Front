cdf4e0cd475b6967e9327b9f0c8a6bc3950c142d
eduardnol <eduardo.nolla@outlook.com>
2022-02-09 13:32:54 +0100

    Add users page

--- src/components/add_user/AddUser.vue
+++ src/components/add_user/AddUser.vue
@@ -1,34 +1,63 @@
 <template>
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+  <!-- Button trigger modal -->
+  <button
+    type="button"
+    class="btn btn-primary"
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal"
+  >
+    Launch demo modal
+  </button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title color" id="exampleModalLabel">
+            Añadir un nuevo usuario
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <AddUserFields />
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
-
 </template>
 <script>
+import AddUserFields from "../../components/add_user/AddUserFields.vue";
 export default {
-  setup() {
-    
+  setup() {},
+  components: {
+    AddUserFields,
   },
-}
+};
 </script>
-<style scoped></style>
+<style scoped>
+.modal-dialog {
+  color: black;
+}
+</style>
---
+++ src/components/add_user/AddUserFields.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="allfilters">
+    <p>Nombre</p>
+    <input type="text" v-model="name" placeholder="Nombre" />
+    <p>Apellido1</p>
+    <input type="text" v-model="surname" placeholder="Apellido" />
+    <p>Apellido2</p>
+    <input type="text" v-model="surname2" placeholder="Apellido2" />
+    <p>Cumpleaños</p>
+    <input v-model="birthday" type="date" />
+    <p>Fecha</p>
+    <input v-model="saint" type="date" />
+    <p>Posicion</p>
+    <input type="text" v-model="tags" placeholder="tag1, tag2, tag3" />
+  </div>
+</template>
+<script>
+export default {
+  name: "AddUserFields",
+  data() {
+    return {
+      name: "",
+      surname: "",
+      surname2: "",
+      birthday: "",
+      saint: "",
+      tags: "",
+    };
+  },
+};
+</script>
+<style scoped>
+.allfilters {
+  display: grid;
+  grid-template-columns: repeat(2, 180px);
+  grid-column-gap: 100px;
+  grid-row-gap: 50px;
+}
+.field {
+  display: flex;
+  flex-direction: row;
+}
+</style>
