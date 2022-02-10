fe5bfb12587607ecbb9318ebf70890c89459d959
eduardnol <eduardo.nolla@outlook.com>
2022-02-10 12:42:43 +0100

    Added component to edit specific user

--- src/pages/user_specific/UserSpecific.vue
+++ src/pages/user_specific/UserSpecific.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>User</div>
+  <div><AddUserFields /></div>
 </template>
 <script>
+import AddUserFields from "../../components/add_user/AddUserFields.vue";
 export default {
   name: "UserSpecific",
+  components: { AddUserFields },
 };
 </script>
 <style lang="stylus" scoped></style>
