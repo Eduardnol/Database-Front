94ba9605f411c74c7380de9399c29e44c0a5d716
Eduardo <eduardnol@gmail.com>
2022-03-26 14:39:13 +0100

    Bug with comma solved

--- src/store/store.js
+++ src/store/store.js
@@ -1,76 +1,91 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 export const store = createStore({
-    state() {
-        return {
-            person: {
-                id: null,
-                nombre: "",
-                apellido: "",
-                apellido2: "",
-                email: "",
-                birthday: "",
-                saint: "",
-                dni: "",
-                sacraments: { bautismo: false, comunion: false, confirmacion: false, matrimonio: false },
-                extras: [{ id: 0, selected: "", name: "", tagname: "" }],
-                fileStorage: [{ name: null, url: null }],
+        state() {
+            return {
+                person: {
+                    id: null,
+                    nombre: "",
+                    apellido: "",
+                    apellido2: "",
+                    email: "",
+                    birthday: "",
+                    saint: "",
+                    dni: "",
+                    sacraments: {
+                        bautismo: false,
+                        comunion: false,
+                        confirmacion: false,
+                        matrimonio: false
+                    },
+                    extras: [{id: 0, selected: "", name: "", tagname: ""}],
+                    fileStorage: [{name: null, url: null}],
+                },
+                persons: [{
+                    id: null,
+                    nombre: "",
+                    apellido: "",
+                    apellido2: "",
+                    email: "",
+                    birthday: "",
+                    saint: "",
+                    dni: "",
+                    sacraments: {
+                        bautismo: false,
+                        comunion: false,
+                        confirmacion: false,
+                        matrimonio: false
+                    },
+                    extras: [{id: 0, selected: "", name: "", tagname: ""}],
+                    fileStorage: [{name: null, url: null}],
+                }]
+            }
+
+        },
+        getters: {
+            getArrItem: (state) => (id) => {
+                return state.persons.find(item => item.id === id);
+            }
+        },
+        mutations: {
+            insertUser(state, person) {
+                state.person = person
             },
-            persons: [{
-                id: null,
-                nombre: "",
-                apellido: "",
-                apellido2: "",
-                email: "",
-                birthday: "",
-                saint: "",
-                dni: "",
-                sacraments: { bautismo: false, comunion: false, confirmacion: false, matrimonio: false },
-                extras: [{ id: 0, selected: "", name: "", tagname: "" }],
-                fileStorage: [{ name: null, url: null }],
-            }]
+            deleteUser(state) {
+                state.person.id = null
+                state.person.nombre = ""
+                state.person.apellido = ""
+                state.person.apellido2 = ""
+                state.person.email = ""
+                state.person.birthday = ""
+                state.person.saint = ""
+                state.person.dni = ""
+                state.person.sacraments = {
+                    bautismo: false,
+                    comunion: false,
+                    confirmacion: false,
+                    matrimonio: false
+                }
+                state.person.extras = [{id: 0, selected: "", name: "", tagname: ""}]
+                state.person.fileStorage = [{name: null, url: null}]
+            },
+            addUser(state, person) {
+                state.persons.addUser(person)
+            },
+            deleteFromArray(state, person) {
+                const i = state.persons.map(item => item.id).indexOf(person.id);
+                state.persons.splice(i, 1);
+            },
+            deleteFromExtraArray(state, extra) {
+
+                const i = state.persons.extras.map(item => item.id).indexOf(extra.id);
+                state.persons.splice(i, 1);
+            },
+            addAnExtraField(state, person) {
+                state.person.extra = person
+            }
+
+
         }
-
-    },
-    getters: {
-        getArrItem: (state) => (id) => {
-            return state.persons.find(item => item.id === id);
-        }
-    },
-    mutations: {
-        insertUser(state, person) {
-            state.person = person
-        },
-        deleteUser(state) {
-            state.person.id = null,
-                state.person.nombre = "",
-                state.person.apellido = "",
-                state.person.apellido2 = "",
-                state.person.email = "",
-                state.person.birthday = "",
-                state.person.saint = "",
-                state.person.dni = "",
-                state.person.sacraments = { bautismo: false, comunion: false, confirmacion: false, matrimonio: false },
-                state.person.extras = [{ id: 0, selected: "", name: "", tagname: "" }],
-                state.person.fileStorage = [{ name: null, url: null }],
-        },
-        addUser(state, person) {
-            state.persons.addUser(person)
-        },
-        deleteFromArray(state, person) {
-            const i = state.persons.map(item => item.id).indexOf(person.id);
-            state.persons.splice(i, 1);
-        },
-        deleteFromExtraArray(state, extra) {
-
-            const i = state.persons.extras.map(item => item.id).indexOf(extra.id);
-            state.persons.splice(i, 1);
-        },
-        addAnExtraField(state, person) {
-            state.person.extra = person
-        }
-
-
     }
-}
 );
