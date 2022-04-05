bdf933e747336308cf109a51bb4921a775470e75
eduardnol <eduardo.nolla@outlook.com>
2022-04-05 11:57:06 +0200

    Delete User confirmation

--- src/pages/user_specific/UserSpecific.vue
+++ src/pages/user_specific/UserSpecific.vue
@@ -43,11 +43,34 @@
     </button>
     <button
         class="btn btn-danger m-1"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
         type="button"
-        @click="deleteuser"
     >
       <i class="bi bi-trash3"></i> Eliminar
     </button>
+  </div>
+  <div class="modal" id="exampleModal" aria-labelledby="exampleModalLabel" tabindex="-1"
+       aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Confirmar Eliminación</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal"
+                  aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Desea eliminar este usuario?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-danger"
+                  @click="deleteuser"
+                  data-bs-dismiss="modal">Eliminar
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
