ba3c4f8348226e26b2cd61212c653a24935cc520
Eduardo <eduardnol@gmail.com>
2022-06-18 10:24:07 +0200

    Creation of a ListPicker.vue

---
+++ src/components/list_picker/ListPicker.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <p>All Users</p>
+    <div class="userfields searchInput">
+      <ais-instant-search :search-client="searchClient" index-name="users">
+        <ais-configure :hits-per-page.camel="6"/>
+        <ais-search-box class="searchbox">
+          <template v-slot="{isSearchStalled, refine}">
+            <input
+                class="form-control"
+                placeholder="Search... "
+                type="search"
+                @input="refine($event.currentTarget.value)"
+            />
+            <span :hidden="!isSearchStalled">Loading...</span>
+          </template>
+        </ais-search-box>
+        <ais-hits>
+          <template v-slot="{ items }">
+            <ul class="person_grid">
+              <li
+                  v-for="person in items"
+                  :key="person.id"
+                  class="list_item"
+              >
+                <MiniPerson
+                    :id="person.id"
+                    :apellido="person.apellido"
+                    :apellido2="person.apellido2"
+                    :birthday="new Date(person.birthday)"
+                    :email="person.email"
+                    :nombre="person.nombre"
+                    @click="addToSelected(person)"
+                />
+              </li>
+            </ul>
+          </template>
+        </ais-hits>
+      </ais-instant-search>
+    </div>
+  </div>
+  <div>
+    <p>Selected Users</p>
+    <ul class="person_grid">
+      <li
+          v-for="person in selected"
+          :key="person.id"
+          class="list_item"
+      >
+        <MiniPerson
+            :id="person.id"
+            :apellido="person.apellido"
+            :apellido2="person.apellido2"
+            :nombre="person.nombre"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListPicker",
+  data() {
+    return {
+      selected: [],
+    }
+  },
+  methods: {
+    addToSelected(person) {
+      this.selected.push(person);
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
