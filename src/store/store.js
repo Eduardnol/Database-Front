267f797c5d53c2373d4e63416748532aa197671c
Eduardo <eduardnol@gmail.com>
2023-05-02 17:23:50 +0200

    Added dicipulado menores json efficiency improvements

--- src/store/store.js
+++ src/store/store.js
@@ -5,68 +5,78 @@ export const store = createStore({
             return {
                 person: {
                     id: null,
-                    nombre: "",
-                    apellido: "",
-                    apellido2: "",
-                    email: "",
-                    birthday: "",
-                    saint: "",
-                    dni: "",
+                    nombre: null,
+                    apellido: null,
+                    apellido2: null,
+                    email: null,
+                    birthday: null,
+                    saint: null,
+                    dni: null,
                     sacraments: {
                         bautismo: false,
                         comunion: false,
                         confirmacion: false,
                         matrimonio: false
                     },
-                    extras: [{id: null, selected: "", name: "", tagname: ""}],
+                    extras: [{
+                        id: null,
+                        selected: null,
+                        name: null,
+                        tagname: null
+                    }],
                     fileStorage: [{name: null, url: null}],
                     createdOn: null,
                 },
                 persons: [{
                     id: null,
-                    nombre: "",
-                    apellido: "",
-                    apellido2: "",
-                    email: "",
-                    birthday: "",
-                    saint: "",
-                    dni: "",
+                    nombre: null,
+                    apellido: null,
+                    apellido2: null,
+                    email: null,
+                    birthday: null,
+                    saint: null,
+                    dni: null,
                     sacraments: {
                         bautismo: false,
                         comunion: false,
                         confirmacion: false,
                         matrimonio: false
                     },
-                    extras: [{id: null, selected: "", name: "", tagname: ""}],
+                    extras: [{
+                        id: null,
+                        selected: null,
+                        name: null,
+                        tagname: null
+                    }],
                     fileStorage: [{name: null, url: null}],
                     createdOn: null,
                 }],
                 lifeteens: [{
                     id: null,
-                    title: "",
-                    responsables: [{id: null, nombre: "", apellido: ""}],
+                    title: null,
+                    responsables: [{id: null, nombre: null, apellido: null}],
                     startDate: "2001-01-20",
                     numInscritos: 0,
-                    idMonitores: [{id: null, nombre: "", apellido: ""}],
-                    idInscritos: [{id: null, nombre: "", apellido: ""}],
+                    idMonitores: [{id: null, nombre: null, apellido: null}],
+                    idInscritos: [{id: null, nombre: null, apellido: null}],
                     subgrupos: [{
-                        titulo: "",
-                        descripcion: "",
+                        titulo: null,
+                        descripcion: null,
                         responsables: null,
                         integrantes: null,
                     }],
                 }],
                 lifeteen: {
                     id: null,
-                    title: "",
-                    responsables: [{id: null, nombre: "", apellido: ""}],
+                    title: null,
+                    responsables: [{id: null, nombre: null, apellido: null}],
                     startDate: "2001-01-20",
                     numInscritos: 0,
-                    idMonitores: [{id: null, nombre: "", apellido: ""}],
-                    idInscritos: [{id: null, nombre: "", apellido: ""}],
+                    idMonitores: [{id: null, nombre: null, apellido: null}],
+                    idInscritos: [{id: null, nombre: null, apellido: null}],
                     subgrupos: [{
-                        titulo: "",
-                        descripcion: "",
+                        titulo: null,
+                        descripcion: null,
                         responsables: null,
                         integrantes: null,
                     }],
@@ -107,13 +117,13 @@ export const store = createStore({
             },
             deleteUser(state) {
                 state.person.id = null
-                state.person.nombre = ""
-                state.person.apellido = ""
-                state.person.apellido2 = ""
-                state.person.email = ""
-                state.person.birthday = ""
-                state.person.saint = ""
-                state.person.dni = ""
+                state.person.nombre = null
+                state.person.apellido = null
+                state.person.apellido2 = null
+                state.person.email = null
+                state.person.birthday = null
+                state.person.saint = null
+                state.person.dni = null
                 state.person.createdOn = null
                 state.person.sacraments = {
                     bautismo: false,
@@ -121,7 +131,12 @@ export const store = createStore({
                     confirmacion: false,
                     matrimonio: false
                 }
-                state.person.extras = [{id: null, selected: "", name: "", tagname: ""}]
+                state.person.extras = [{
+                    id: null,
+                    selected: null,
+                    name: null,
+                    tagname: null
+                }]
                 state.person.fileStorage = [{name: null, url: null}]
             },
             deleteFromArray(state, person) {
@@ -133,7 +148,8 @@ export const store = createStore({
                 state.person.extras.splice(fieldId, 1);
             },
             addAnExtraField(state) {
-                state.person.extras.push({id: null, selected: "", name: "", tagname: ""})
+                state.person.extras.push(
+                    {id: null, selected: null, name: null, tagname: null})
             },
             //***********************Update View***********************//
             updateView(state, person) {
