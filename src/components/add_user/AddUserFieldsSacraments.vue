e95686014eae6d2e3d6604e360ff40462630f93a
Eduardo <eduardnol@gmail.com>
2023-08-24 19:33:43 +0200

    Datetime modification

--- src/components/add_user/AddUserFieldsSacraments.vue
+++ src/components/add_user/AddUserFieldsSacraments.vue
@@ -5,8 +5,7 @@
         <div class="card-body">
           <h5 class="card-title">{{ sacraments.name }}</h5>
           <h6 class="card-subtitle mb-2 text-body-secondary">{{ sacraments.temple }}</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the
-            bulk of the card's content.</p>
+          <h6 class="card-subtitle mb-2 text-body-secondary">{{ sacraments.date }}</h6>
         </div>
       </div>
     </div>
